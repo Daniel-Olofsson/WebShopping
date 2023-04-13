8ad9c77d7caf0ec45ab35ef843ae3533f5f1f76d
Daniel-Olofsson <daniel-olofsson@hotmail.se>
2023-04-13 19:12:01 +0200

    Add project files.

---
+++ Data/WebShoppingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebShopping.Models;
+
+namespace WebShopping.Data
+{
+    public class WebShoppingContext : DbContext
+    {
+        public WebShoppingContext (DbContextOptions<WebShoppingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebShopping.Models.Product> Product { get; set; } = default!;
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebShopping.Data;
+using System;
+using System.Linq;
+using WebShopping.Models;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new WebShoppingContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<WebShoppingContext>>()))
+        {
+            // Look for any movies.
+            if (context.Product.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Product.AddRange(
+                new Product
+                {
+                    Name = "When Harry Met Sally",
+                    Description = "harry-boy",
+                    Price = 7.99M
+                },
+                new Product
+                {
+                    Name = "bnla bla bla",
+                    Description = "hfsdfsdfsdarry-boy",
+                    Price = 1.99M
+                }
+                
+            );
+            context.SaveChanges();
+        }
+    }
+}
