d1aaa0533ff4e1b4dfcab57e1de5d3b3cc416ee1
Daniel-Olofsson <daniel-olofsson@hotmail.se>
2023-04-20 09:10:43 +0200

    first commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShopping.Models;
+
+//namespace WebShopping.Controllers
+//{
+//    public class AccountController
+//    {
+//        public ActionResult Login()
+//        {
+//            var model = new LoginModel();
+//            return View(model);
+//        }
+
+//        [HttpPost]
+//        public ActionResult Login(LoginModel model)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return View(model);
+//            }
+
+//            // Use Entity Framework to check if the user exists in the database
+//            // and log them in if they do.
+
+//            return RedirectToAction("Index", "Home");
+//        }
+//    }
+//}
